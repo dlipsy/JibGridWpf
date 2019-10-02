37f3d1380b3c8cd034377c23f65e96cdcfa59fd5
DEWPOINT\DLipsy <dlipsy@dewpoint.com>
2019-10-02 11:37:59 -0400

    styling / updates

---
+++ Jib.WPF.Controls/MVVM/NullableBoolToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Jib.WPF.Controls.Mvvm
+{
+    [ValueConversion(typeof(bool?), typeof(Visibility))]
+    public class NullableBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool setValue = false;
+            if (value == null)
+                return Visibility.Collapsed;
+            else if (bool.TryParse(value.ToString(), out setValue) && setValue)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
--- Jib.WPF.Testbed/MainWindow.xaml.cs
+++ Jib.WPF.Testbed/MainWindow.xaml.cs
@@ -12,13 +12,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
 
 namespace Jib.WPF.Testbed
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
